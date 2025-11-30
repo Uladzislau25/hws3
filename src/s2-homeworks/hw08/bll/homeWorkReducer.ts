5a0d860d97e7418b87beb56a9c4d3799552e7d2c
Uladzislau <u.shylko25@gmail.com>
2025-11-30 23:59:49 +0300

    feat: add finish hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,26 +4,25 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
+        case "sort": {
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1;   // a после b
+                if (a.name < b.name) return -1;  // a перед b
+                return 0;                        // равны
+            });
 
-            return [...state].sort((a: UserType, b: UserType)=> {
-            if(a.name > b.name) {
-                return action.payload === 'up'? 1: -1;
-            }
-            if(a.name < b.name) {
-                return action.payload === 'down'? -1: 1;
-            }
-            return 0;
-            })
-            // need to fix
-        }
-        case 'check': {
+            if (action.payload === "down") newState.reverse(); // для обратного порядка
 
-            return state.filter(el => el.age >= 18) // need to fix
+            return newState;
         }
+
+        case "check":
+            return state.filter(u => u.age >= action.payload);
+
         default:
-            return state
+            return state;
     }
-}
+};
+
